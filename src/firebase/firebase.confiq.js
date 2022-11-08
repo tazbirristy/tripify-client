9f52c87cf4070f6e83236fe012e7e23455afd242
tazbirristy <tazbirristyweb@gmail.com>
2022-11-09 01:49:41 +0600

    firebase config

---
+++ src/firebase/firebase.confiq.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPFNqhKKg73UIPbs54nvDfVXP_tOUIDq4",
+  authDomain: "tripify-9f444.firebaseapp.com",
+  projectId: "tripify-9f444",
+  storageBucket: "tripify-9f444.appspot.com",
+  messagingSenderId: "467985588727",
+  appId: "1:467985588727:web:cf16e4ebe964376968cef2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
