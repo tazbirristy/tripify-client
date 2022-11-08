395814dfd110853d085bff8966047551583fadfd
tazbirristy <tazbirristyweb@gmail.com>
2022-11-09 03:49:03 +0600

    error page done

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.png";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -6,41 +8,27 @@ const Header = () => {
     <div className="bg-purple-900">
       <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div className="relative flex items-center justify-between">
-          <a
-            href="/"
-            aria-label="Company"
-            title="Company"
+          <Link
+            to="/"
+            aria-label="Tripify"
+            title="Tripify"
             className="inline-flex items-center"
           >
-            <svg
-              className="w-8 text-teal-accent-400"
-              viewBox="0 0 24 24"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeMiterlimit="10"
-              stroke="currentColor"
-              fill="none"
-            >
-              <rect x="3" y="1" width="7" height="12" />
-              <rect x="3" y="17" width="7" height="6" />
-              <rect x="14" y="1" width="7" height="6" />
-              <rect x="14" y="11" width="7" height="12" />
-            </svg>
-            <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
-              Company
+            <img className="w-2/12" src={logo} alt="" />
+            <span className="ml-2 text-2xl font-bold tracking-wide text-gray-100 uppercase">
+              Tripify
             </span>
-          </a>
+          </Link>
           <ul className="flex items-center hidden space-x-8 lg:flex">
             <li>
-              <a
-                href="/"
-                aria-label="Our product"
-                title="Our product"
+              <Link
+                to="/"
+                aria-label="Home"
+                title="Home"
                 className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
               >
-                Product
-              </a>
+                Home
+              </Link>
             </li>
             <li>
               <a
--- src/routes/Routes/Routes.js
+++ src/routes/Routes/Routes.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import Error from "../../components/Error/Error";
 import Home from "../../components/Home/Home/Home";
 import Main from "../../layouts/Main/Main";
 
@@ -6,6 +7,7 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <Error></Error>,
     children: [
       {
         path: "/",
