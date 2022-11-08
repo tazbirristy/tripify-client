7176222d80c7a69aa67b5d0f9603be31c1a82b4b
tazbirristy <tazbirristyweb@gmail.com>
2022-11-09 02:49:57 +0600

    playstore download section done

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import PlayStore from "../PlayStore/PlayStore";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <PlayStore></PlayStore>
     </div>
   );
 };
