6577291152d84a62faf8dbe85b3f2ea899233cf4
tazbirristy <tazbirristyweb@gmail.com>
2022-11-10 02:31:00 +0600

    loader done

---
+++ src/Loader/Loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div>
+      <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-purple-600 mx-auto"></div>
+    </div>
+  );
+};
+
+export default Loader;
--- src/components/Login/Register/Register.js
+++ src/components/Login/Register/Register.js
@@ -16,22 +16,22 @@ const Register = () => {
     const photoURL = form.photoURL.value;
     const email = form.email.value;
     if (!/\S+@\S+\.\S+/.test(email)) {
-      setError("Please give a valid email");
+      setError("Required Valid Email");
       return;
     } else {
       setError("");
     }
     const password = form.password.value;
     if (!/(?=.{8,})/.test(password)) {
-      setError("Password should be 8 characters");
+      setError("Password must be 8 characters");
       return;
     }
     if (!/(?=.*[a-zA-Z])/.test(password)) {
-      setError("you Must give one uppercase");
+      setError("Password must consist one uppercase");
       return;
     }
     if (!/(?=.*[!#$@%^&*? "])/.test(password)) {
-      setError("You should provide one special character");
+      setError("Password must consist one special character");
       return;
     } else {
       setError("");
@@ -69,7 +69,7 @@ const Register = () => {
       .then((result) => {
         const user = result.user;
         console.log(user);
-        toast.success("successfully login", { autoClose: 500 });
+        toast.success("Successfully logged In", { autoClose: 500 });
       })
       .catch((error) => {
         console.error(error);
@@ -88,7 +88,7 @@ const Register = () => {
           <div className="w-full max-w-md p-8 space-y-3 rounded-xl dark:bg-gray-900 dark:text-gray-100 mt-10">
             <h1 className="text-3xl font-bold text-center">Register</h1>
 
-            <p className="text-red-500 my-3 text-center">{error}</p>
+            <p className="text-red-600 my-3 text-center">{error}</p>
             <form
               onSubmit={handleSubmit}
               className="space-y-6 ng-untouched ng-pristine ng-valid"
@@ -102,7 +102,7 @@ const Register = () => {
                   name="name"
                   id="userName"
                   placeholder="Enter Your Name"
-                  className="w-full px-4 py-3 rounded-md border-gray-500 dark:bg-gray-800 dark:text-gray-100 focus:border-violet-400"
+                  className="w-full px-4 py-3 rounded-md border-gray-500 dark:bg-gray-800 dark:text-gray-100 focus:border-purple-600"
                 />
               </div>
               <div className="space-y-1 text-sm">
@@ -114,7 +114,7 @@ const Register = () => {
                   name="photoURL"
                   id="imageUrl"
                   placeholder="Enter Your Image URL"
-                  className="w-full px-4 py-3 rounded-md border-gray-500 dark:bg-gray-800 dark:text-gray-100 focus:border-violet-400"
+                  className="w-full px-4 py-3 rounded-md border-gray-500 dark:bg-gray-800 dark:text-gray-100 focus:border-purple-600"
                 />
               </div>
               <div className="space-y-1 text-sm">
@@ -126,7 +126,7 @@ const Register = () => {
                   name="email"
                   id="userEmail"
                   placeholder="Enter Your Email"
-                  className="w-full px-4 py-3 rounded-md border-gray-500 dark:bg-gray-800 dark:text-gray-100 focus:border-violet-400"
+                  className="w-full px-4 py-3 rounded-md border-gray-500 dark:bg-gray-800 dark:text-gray-100 focus:border-purple-600"
                 />
               </div>
               <div className="space-y-1 text-sm">
@@ -138,10 +138,10 @@ const Register = () => {
                   name="password"
                   id="password"
                   placeholder="Enter Your Password"
-                  className="w-full px-4 py-3 rounded-md border-gray-500 dark:bg-gray-800 dark:text-gray-100 focus:border-violet-400"
+                  className="w-full px-4 py-3 rounded-md border-gray-500 dark:bg-gray-800 dark:text-gray-100 focus:border-purple-600"
                 />
               </div>
-              <button className="block w-full p-3 text-center rounded-sm dark:text-gray-200 bg-indigo-600 hover:bg-indigo-500">
+              <button className="block w-full p-3 text-center rounded-sm dark:text-gray-200 bg-purple-800 hover:bg-purple-500">
                 Register
               </button>
             </form>
@@ -161,7 +161,7 @@ const Register = () => {
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 32 32"
-                  className="w-7 h-7 fill-current text-gray-200 hover:text-indigo-400"
+                  className="w-7 h-7 fill-current text-gray-200 hover:text-purple-600"
                 >
                   <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
                 </svg>
